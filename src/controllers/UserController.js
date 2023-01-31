e21e0199ce2690182be891b1d8d382bdc1742604
bruno <brunoedua21@gmail.com>
2023-01-31 14:39:36 -0400

    add express async errors

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,14 @@
+const AppError = require('../utils/AppError');
+
 class UserController {
     create(request, response) {
         const { name, email, password } = request.body
 
         response.status(201).json({ name, email, password })
+
+        if(!name) {
+            throw new AppError('Name is required')
+        }
     }
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,17 +7,6 @@ const userController = new UserController()
 
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next) {
-    console.log("você passou pelo mid")
-    const { isAdmin } = request.body
-
-    if(!isAdmin) {
-        return response.json({'message': 'User unauthorized'})
-    }
-
-    next() 
-}
-usersRoutes.use(myMiddleware)
 usersRoutes.post('/', userController.create)
 
 module.exports = usersRoutes
